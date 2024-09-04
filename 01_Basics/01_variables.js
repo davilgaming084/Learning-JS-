a4d625dd6d0cf84343ca6a48dadae60e844c08e9
Satish <davilgaming084@gmail.com>
2024-09-04 14:49:44 +0530

    basic add

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+// Variables Declaration 
+// const, let, var
+
+
+// const is use for constant value  means it can't be change modify or redeclare
+// let is use for variable value  means it can be change modify or redeclare
+//var -> dont use  var becouse of scoping problem
+// is use for variable value  means it can be change modify or redeclare 
+
+const accountId = 144553 
+let accountEmial = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "New Delhi"
+
+//accountId = 1 // not allowed  because of const
+accountEmial = "bca@gmail.com"
+accountPassword = "212121"
+// console.log(accountId);
+console.table([accountId,accountEmial,accountPassword]);
