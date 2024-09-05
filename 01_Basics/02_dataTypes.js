02283e9bb6066b2fa751ba6bf2ed7fd378dd3004
Satish <davilgaming084@gmail.com>
2024-09-05 21:50:24 +0530

    learn about datatype

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"// treate all js code as newer version
+
+// alert(3+3)// we are usinhg node js not browser
+
+// console.log(3+3);
+// console.log("Satish");
+
+//data types 
+
+//Primitive Data Types
+//string,=> "Satish"
+//number => 1234
+//boolean=>true/false
+//null => stand alone value /datatype
+//undifind => declare var but didn't give them value
+//symbol=>use in react
+//bigint => use for large number
+
+// non Primitive Data Types
+// object
+//array
+// function
+
+
+
+//typeof 
+// null => object
+
+console.log(typeof null);// object
+console.log(typeof undefined);// undifind
