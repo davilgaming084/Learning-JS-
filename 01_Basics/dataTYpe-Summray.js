5dc3b21d48f8a98a3f8a54d6400d2a804ad0eb66
Satish <davilgaming084@gmail.com>
2024-09-06 22:41:54 +0530

    DataType Summary

--- 01_Basics/dataTYpe-Summray.js
+++ 01_Basics/dataTYpe-Summray.js
@@ -1,44 +1,49 @@
-// There are two type of datatype
-// Primitive 
-// Non Primitive
+// There are two types of datatypes:
+// 1. Primitive 
+// 2. Non-Primitive
 
+// Primitive / Call by value -> means copy of value 
+// - string
+// - number
+// - boolean
+// - null
+// - undefined
+// - bigint
+// - symbol
 
-// primitive / call by vale -> means copy of value 
-// string
-// number
-// boolen
-// null
-// undifind
-//bigint=>
-    let bignumber = 1234567812345n
-// symbol => 
-let id = Symbol('123')
-let anotherid = Symbol("123")
-// console.log(id === anotherid);
-// non primitive / refrence type
-// array
-// object
-// function
+// Examples:
+let bigNumber = 1234567812345n; // bigint
+let id = Symbol('123');
+let anotherId = Symbol("123");
+// console.log(id === anotherId);
 
+// Non-primitive / Reference type
+// - array
+// - object
+// - function
 
-// array
-let heros = ['shaktiman',"naggraj","doga"] // it is array 
-// obj
+// Examples:
+// Array
+let heroes = ['Shaktiman', "Nagraj", "Doga"];
+
+// Object
 let student = {
-    name:"shaktima",
+    name: "Shaktiman",
     class: 6
-}
-// func
- let mydunc = function(params) {
-    console.log("helo world");
+};
+
+// Function
+function myFunc(params) {
+    console.log("Hello world");
 }
 
 function name(params) {
-    
+    // Function body
 }
-// arrow func
-()=>{}
 
-// js is dynamically Typed Language
-console.log(typeof bignumber);
-console.log(typeof null ); // object 
+// Arrow function
+() => {};
+
+// JavaScript is a dynamically typed language
+console.log(typeof bigNumber);
+console.log(typeof null); // object
