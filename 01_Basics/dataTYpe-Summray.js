f581b9812e89bb29c0567abfc4b2193b895e9cf3
Satish <davilgaming084@gmail.com>
2024-09-06 22:37:17 +0530

    dataType summry

---
+++ 01_Basics/dataTYpe-Summray.js
@@ -0,0 +1,44 @@
+// There are two type of datatype
+// Primitive 
+// Non Primitive
+
+
+// primitive / call by vale -> means copy of value 
+// string
+// number
+// boolen
+// null
+// undifind
+//bigint=>
+    let bignumber = 1234567812345n
+// symbol => 
+let id = Symbol('123')
+let anotherid = Symbol("123")
+// console.log(id === anotherid);
+// non primitive / refrence type
+// array
+// object
+// function
+
+
+// array
+let heros = ['shaktiman',"naggraj","doga"] // it is array 
+// obj
+let student = {
+    name:"shaktima",
+    class: 6
+}
+// func
+ let mydunc = function(params) {
+    console.log("helo world");
+}
+
+function name(params) {
+    
+}
+// arrow func
+()=>{}
+
+// js is dynamically Typed Language
+console.log(typeof bignumber);
+console.log(typeof null ); // object 
