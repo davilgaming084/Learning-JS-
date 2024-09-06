bb32c12263149b58dde0814c9476df856643fa9d
Satish <davilgaming084@gmail.com>
2024-09-06 14:51:14 +0530

    converstion post/pre fix

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -24,8 +24,8 @@ let valueInNumber = Number(score)// converting string into number
 
 
 
-//////////  convert in bollean
-// let isloggedin = 1
+////////// Convert to boolean
+// let isLoggedIn = 1
 let isLoggedIn = null
 
 let convertNumberToBoolean = Boolean(isLoggedIn)
@@ -35,13 +35,51 @@ let convertNumberToBoolean = Boolean(isLoggedIn)
 
 // 1 => true
 // "abc" => true
-// ''=> false
+// '' => false
 // null => false
 
 ////////////  convert in string
 let someNumber = 33
 let strNumber = String(someNumber)
-console.log(strNumber);
-console.log( typeof strNumber);
+// console.log(strNumber);
+// console.log( typeof strNumber);
 
+
+///////////////////////// Operations 
+let value = 3 
+let negValue = -value
+// console.log(negValue);
+
+///// Arthmatic opration
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello "
+let str = "satish"
+let str3 = str1 + str 
+// console.log(str3);
+
+
+
+// console.log(("1" + 2));// not prefer
+// console.log(1 + "2");// not prefer
+// console.log("1" + 2 +2 );// not prefer
+// console.log(1+2+"2");// not prefer
+
+// console.log(+true);// not prefer
+// console.log(+"");/// not prefer
+
+
+let num1 = num2 = num3
+num1 =num2 = num3 = 2+2 // not prefer
+
+// pre/post increment 
+let gaemcounter = 100
+gaemcounter++
+console.log(gaemcounter);
 
