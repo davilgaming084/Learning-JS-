dab867e76f93ab2916ee94426f321e05f8eb9298
Satish <davilgaming084@gmail.com>
2024-09-09 23:20:57 +0530

    String Creation many ways

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,12 @@
+let name1 = " satish ";
+let repoCount = 15;
+
+//  console.log(name1  + ' ' +  repoCount);  concat method but its old  use new string intorpulation 
+
+
+/// string intorpulation
+// console.log(`helo my name is ${name1.toUpperCase()} and my repo  is ${repoCount}`);
+
+// declare string/ variable obj/constractor way
+let gameName = new String('hy') // 1. When you use new String('hy'), you're creating a String object.
+console.log(gameName[0]); // you can acsess  useing index value
