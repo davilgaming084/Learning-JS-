7af6920aaa142bac01f64cb73f22af187375fc9e
Satish <davilgaming084@gmail.com>
2024-09-11 23:09:24 +0530

    String / inturpulation / methods

--- 01_Basics/05_Strings.js
+++ 01_Basics/05_Strings.js
@@ -29,18 +29,27 @@ let b = "abc "
 let c = "ABC  "
 // console.log(c.toLowerCase()); // "abc " convert lowercase
 //-----------------------------------------------------------------------------------------------
-// 4)include = > Find word and latter in variable>string>value  
+// 4)include = > Find any word and  any latter in variable>string>value   
 let d = "Satish abc ef"
 // console.log(e.includes("Satish")); // find/search word / casesensetive
-// console.log(e.includes("ef")); find/search latter / casesensetive
+// console.log(d.includes("ef"));// true  //find/search latter / casesensetive
+// console.log(d.includes("e")); // true // find/search latter / casesensetive
 //-----------------------------------------------------------------------------------------------
-// 5 )stratWith => find lettrs/word in the starting only 1 starting word or letter
+// 5 )stratWith => find lettrs/word in the start of  starting   /only 1 starting word or letter / also it is casesensitive 
 let e = "Satish saroj"
 // console.log(e.startsWith("Satish"));// true
 // console.log(e.startsWith("saroj"));// false
 // console.log(e.startsWith("Sa"));// true
 // console.log(e.startsWith("sa"));// false
 //-----------------------------------------------------------------------------------------------
-
-
+// 6) endsWith = > find/search word/letter in string in last of string / only 1 starting word or letter /  also it is casesensitive 
+let f =  "Satish saroj"
+// console.log(f.endsWith("saroj")); // true
+// console.log(f.endsWith("oj")); // true
+//-----------------------------------------------------------------------------------------------
+// 7)  search => find/search ( only latters) and return index => work as include but is return index not true false like includes 
+let g = "Satish saroj"
+// console.log(g.search("Satish")) // 0 it not word on word 
+// console.log(g.search("s")) // index 4 fron "Satish"  is find first s in word 
+// console.log(g.search("s")) // index 4  i want from "saroj" but retuen from "Satish"  // higher posiblity find s who write on start first 
 //String / inturpulation / methods
