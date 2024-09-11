efbe8601f6bf8f37d430bc5db96cbc91f17918b7
Satish <davilgaming084@gmail.com>
2024-09-12 01:20:42 +0800

    String / inturpulation / methods

--- 01_Basics/05_Strings.js
+++ 01_Basics/05_Strings.js
@@ -17,20 +17,30 @@ let gameName = new String('Halo');
 // console.log(`Uppercase gameName: ${gameName.toUpperCase()}`);
 
 //String methods
-// length => Get Length Of value it count (_)space  also / length is property os string
+// 1) length => Get Length Of value it count (_)space  also / length is property os string
 let a = "abc "
 // console.log(a.length); // is 4 it also count space 
-// toUpperCase = > Convert variable>string>value  into uppercase
+//-----------------------------------------------------------------------------------------------
+// 2) toUpperCase = > Convert variable>string>value  into uppercase
 let b = "abc "
 // console.log(b.toUpperCase()); // "ABC "  convert uppercase
-// toLowerCase = > Convert variable>string>value into Lowercase
+//-----------------------------------------------------------------------------------------------
+// 3) toLowerCase = > Convert variable>string>value into Lowercase
 let c = "ABC  "
 // console.log(c.toLowerCase()); // "abc " convert lowercase
-// include = > Find word and latter in variable>string>value  
-let e = "Satish abc ef"
+//-----------------------------------------------------------------------------------------------
+// 4)include = > Find word and latter in variable>string>value  
+let d = "Satish abc ef"
 // console.log(e.includes("Satish")); // find/search word / casesensetive
 // console.log(e.includes("ef")); find/search latter / casesensetive
-
+//-----------------------------------------------------------------------------------------------
+// 5 )stratWith => find lettrs/word in the starting only 1 starting word or letter
+let e = "Satish saroj"
+// console.log(e.startsWith("Satish"));// true
+// console.log(e.startsWith("saroj"));// false
+// console.log(e.startsWith("Sa"));// true
+// console.log(e.startsWith("sa"));// false
+//-----------------------------------------------------------------------------------------------
 
 
 //String / inturpulation / methods
