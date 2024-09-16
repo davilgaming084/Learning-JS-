2dbeb89b86f2a627264e8f05a3aa16946a3f71dc
Satish <davilgaming084@gmail.com>
2024-09-16 13:42:09 +0530

    Date/methods

--- 01_Basics/07_Dates.js
+++ 01_Basics/07_Dates.js
@@ -22,14 +22,34 @@ let myCraetedDate3 = new Date("12-01-10") // declear date as  your wish // if dd
 let mytimestemp = Date.now(); 
 // console.log(mytimestemp);// milisecound 
 
-console.log(Math.floor(Date.now() / 1000)); // secound 
-console.log(MyDate.getMonth() + 1); // get month strat from 0 
-console.log(MyDate.getDay()); // get dat // start from 0 
+// console.log(Math.floor(Date.now() / 1000)); // secound 
+// console.log(MyDate.getMonth() + 1); // get month strat from 0 
+// console.log(MyDate.getDay()); // get dat // start from 0 
 
 
-// localstring cosmonise 
-let a = Date.now()
-console.log(a.toLocaleString("default",{
-    
-}));
+// localstring costomise  
+let a = new Date()
+// console.log(a.toLocaleString('default',{month:'long'}));
+
+// methods 
+//Date.getFullYear() => get the year (4 ditgit)
+//Date.getMonth() => get month (0-11) 0-for jaunary,11-for december
+//Date.getDate() => get the day of the month (1-31)
+//Date.getDay() => get tha day of week (0-6) 0-for-sunday 7-for-saturday
+//Date.getHours()=> get hours (0-23) 0-for 12
+//Date.getMinutes()=> get minutes (0-59)
+//Date.getSecoud()=> get secound (0-59)
+//Date.getMilisecound() => get milisecound (0-999)
+//Date.setFullYear(2024) =>  // set/update the year on your work
+//Date.setMonth(11) => update/set month
+//Date.setDate(21) => update/set day of month
+//Date.setHours(10)=> update/set Hours
+//Date.setMinutes(30) => update/set minutes
+//Date.setSecound(30) => update/set secoound 
+// console.log(MyDate.toString()); // Sun Sep 15 2024 21:25:50 GMT+0530 (India Standard Time)
+// console.log(MyDate.toDateString());// Sun Sep 15 2024
+// console.log(MyDate.toLocaleString());// 15/9/2024, 9:28:06 pm
+
+
+
 
