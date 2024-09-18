fbef67cdc2915b3662f18c3bfd6d4692423e7782
Satish <davilgaming084@gmail.com>
2024-09-18 14:30:22 +0530

    obj-creating- litreal-constructor/ add-symbol-in-obj/add-function in obj / obj-methods

--- 02_Basic/02_object.js
+++ 02_Basic/02_object.js
@@ -1,35 +1,82 @@
-// createing obj way are 2 
-//1 litreal way 
-//2 constructor - always single tun 
+// Creating an object can be done in two ways:
+// 1. Literal way
+// 2. Constructor way (always single use)
 
-const mysymbol =  Symbol("key1")
+const mySymbol = Symbol("key1"); // Create a unique symbol
 
-// litrels way
+// Object literal way
 let obj = {
-    name:"satish", // behind the scene "name":"satish"
-    age:'18',      // ,,
-    location:"delhi", // ,, 
-    email:'dbssc@gmailcom',
-    logday:['mon',"set"],
-    [mysymbol]:"sym"
+    name: "Satish", // Property: name
+    age: '18',      // Property: age
+    location: "Delhi", // Property: location
+    email: 'dbssc@gmail.com', // Property: email
+    logDays: ['Mon', "Sat"], // Property: logDays (corrected spelling)
+    [mySymbol]: "Hello Symbol" // Property with symbol as key
+};
+
+// Accessing object properties in two ways:
+// 1) Dot notation: console.log(obj.name);
+// 2) Bracket notation: console.log(obj["email"]); // Important: keys are strings behind the scenes
+
+// console.log(obj.name);
+// console.log(obj["name"]);
+// console.log(obj[mySymbol]); // accswssing symbol in obj
+// obj.name = "mohit" // updateing 
+
+///// add function in obj =>
+obj.greeting = function() {
+    console.log("helo func");
 }
-// accsessing obj 2 ways
-//1) console.log(obj.name) - don notation accsessing
-//2)console.log(obj["email"]) - squear breaket notation[""] give it in string is importent becous obj key are also consider as string behind the scene//
-//  add symbol in obj 
-// step 1 => craete one var like this => let mySymbol = Symbol("key1")
-// step 2 => craete property in obj like that => [mysymbol]:"helo sym"
-// step 3 => accsessing symbol in obj => console.log(obj[mysymbol]) // symbol accsesybal only on squere notation 
+// add function on obj use obj prop
+obj.greeting2 = function () {
+    console.log(`name of this obj ${this.name}`);
+}
+
+// Adding a symbol to the object:
+// Step 1: Create a variable like this: let mySymbol = Symbol("key1");
+// Step 2: Create a property in the object like this: [mySymbol]: "Hello Symbol";
+// Step 3: Access the symbol in the object: console.log(obj[mySymbol]); // Symbols are only accessible using bracket notation
 
 
-// notes
-// bethind the scenece obj property add like this => 
-    //  {
-    //    "name":"satish", // also key are string 
-    //    "age":"19"
-//      }
+// obj method 
+Object.freeze(obj) // now freez obj you canot change anything
 
-//  add symbol in obj 
-// step 1 => craete one var like this => let mySymbol = Symbol("key1")
-// step 2 => craete property in obj like that => [mysymbol]:"helo sym"
-// step 3 => accsessing symbol in obj => console.log(obj[mysymbol]) // symbol accsesybal only on squere notation 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Notes:
+// Behind the scenes, object properties are added like this:
+// {
+//   "name": "Satish",
+//   "age": "18",
+//   "location": "Delhi",
+//   "email": "dbssc@gmail.com",
+//   "logDays": ["Mon", "Sat"],
+//   "key1": "Hello Symbol" // Symbol keys are not shown in normal object iteration
+// }
+
+// Adding a symbol to the object:
+// Step 1: Create a variable like this: let mySymbol = Symbol("key1");
+// Step 2: Create a property in the object like this: [mySymbol]: "Hello Symbol";
+// Step 3: Access the symbol in the object: console.log(obj[mySymbol]); // Symbols are only accessible using bracket notation
+
+console.log(obj.greeting());
+console.log(obj.greeting2());
